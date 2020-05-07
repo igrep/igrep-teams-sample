a356ba3c92b0b913eeb4c3c05643a88b3312f953
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2020-05-07 18:51:44 +0900

    Rewrite based on https://github.com/microsoftgraph/msgraph-training-nodeexpressapp

---
+++ app.js
@@ -0,0 +1,233 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var flash = require('connect-flash');
+require('dotenv').config();
+
+var passport = require('passport');
+var OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
+
+// Configure passport
+
+// In-memory storage of logged-in users
+// For demo purposes only, production apps should store
+// this in a reliable storage
+var users = {};
+
+// Passport calls serializeUser and deserializeUser to
+// manage users
+passport.serializeUser(function(user, done) {
+  // Use the OID property of the user as a key
+  users[user.profile.oid] = user;
+  done (null, user.profile.oid);
+});
+
+passport.deserializeUser(function(id, done) {
+  done(null, users[id]);
+});
+
+// <ConfigureOAuth2Snippet>
+// Configure simple-oauth2
+const oauth2 = require('simple-oauth2').create({
+  client: {
+    id: process.env.OAUTH_APP_ID,
+    secret: process.env.OAUTH_APP_PASSWORD
+  },
+  auth: {
+    tokenHost: process.env.OAUTH_AUTHORITY,
+    authorizePath: process.env.OAUTH_AUTHORIZE_ENDPOINT,
+    tokenPath: process.env.OAUTH_TOKEN_ENDPOINT
+  }
+});
+// </ConfigureOAuth2Snippet>
+
+// Callback function called once the sign-in is complete
+// and an access token has been obtained
+// <SignInCompleteSnippet>
+async function signInComplete(iss, sub, profile, accessToken, refreshToken, params, done) {
+  if (!profile.oid) {
+    return done(new Error("No OID found in user profile."));
+  }
+
+  try{
+    const user = await graph.getUserDetails(accessToken);
+
+    if (user) {
+      // Add properties to profile
+      profile['email'] = user.mail ? user.mail : user.userPrincipalName;
+    }
+  } catch (err) {
+    return done(err);
+  }
+
+  // Create a simple-oauth2 token from raw tokens
+  let oauthToken = oauth2.accessToken.create(params);
+
+  // Save the profile and tokens in user storage
+  users[profile.oid] = { profile, oauthToken };
+  return done(null, users[profile.oid]);
+}
+// </SignInCompleteSnippet>
+
+// Configure OIDC strategy
+passport.use(new OIDCStrategy(
+  {
+    identityMetadata: `${process.env.OAUTH_AUTHORITY}${process.env.OAUTH_ID_METADATA}`,
+    clientID: process.env.OAUTH_APP_ID,
+    responseType: 'code id_token',
+    responseMode: 'form_post',
+    redirectUrl: process.env.OAUTH_REDIRECT_URI,
+    allowHttpForRedirectUrl: true,
+    clientSecret: process.env.OAUTH_APP_PASSWORD,
+    validateIssuer: false,
+    passReqToCallback: false,
+    scope: process.env.OAUTH_SCOPES.split(' ')
+  },
+  signInComplete
+));
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+var calendarRouter = require('./routes/calendar');
+var graph = require('./graph');
+
+var app = express();
+
+// <SessionSnippet>
+// Session middleware
+// NOTE: Uses default in-memory session store, which is not
+// suitable for production
+app.use(session({
+  secret: 'your_secret_value_here',
+  resave: false,
+  saveUninitialized: false,
+  unset: 'destroy'
+}));
+
+// Flash middleware
+app.use(flash());
+
+// Set up local vars for template layout
+app.use(function(req, res, next) {
+  // Read any flashed errors and save
+  // in the response locals
+  res.locals.error = req.flash('error_msg');
+
+  // Check for simple error string and
+  // convert to layout's expected format
+  var errs = req.flash('error');
+  for (var i in errs){
+    res.locals.error.push({message: 'An error occurred', debug: errs[i]});
+  }
+
+  next();
+});
+// </SessionSnippet>
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// <FormatDateSnippet>
+var hbs = require('hbs');
+var moment = require('moment');
+// Helper to format date/time sent by Graph
+hbs.registerHelper('eventDateTime', function(dateTime){
+  return moment(dateTime).format('M/D/YY h:mm A');
+});
+// </FormatDateSnippet>
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// <AddProfileSnippet>
+app.use(function(req, res, next) {
+  // Set the authenticated user in the
+  // template locals
+  if (req.user) {
+    res.locals.user = req.user.profile;
+  }
+  next();
+});
+// </AddProfileSnippet>
+//
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+const { BotFrameworkAdapter } = require('botbuilder');
+const { EchoBot } = require('./bot');
+
+app.use('/', indexRouter);
+app.use('/auth', authRouter);
+app.use('/calendar', calendarRouter);
+app.use('/users', usersRouter);
+
+// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+const adapter = new BotFrameworkAdapter({
+  appId: process.env.MicrosoftAppId,
+  appPassword: process.env.MicrosoftAppPassword,
+  channelService: process.env.ChannelService,
+  openIdMetadata: process.env.BotOpenIdMetadata
+});
+
+// Catch-all for errors.
+adapter.onTurnError = async (context, error) => {
+  // This check writes out errors to console log .vs. app insights.
+  // NOTE: In production environment, you should consider logging this to Azure
+  //       application insights.
+  console.error(`\n [onTurnError] unhandled error: ${ error }`);
+
+  // Send a trace activity, which will be displayed in Bot Framework Emulator
+  await context.sendTraceActivity(
+    'OnTurnError Trace',
+    `${ error }`,
+    'https://www.botframework.com/schemas/error',
+    'TurnError'
+  );
+
+  // Send a message to the user
+  await context.sendActivity('The bot encounted an error or bug.');
+  await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+};
+
+// Create the main dialog.
+const myBot = new EchoBot();
+
+// Listen for incoming requests.
+app.post('/api/messages', (req, res) => {
+  adapter.processActivity(req, res, async (context) => {
+    // Route to main dialog.
+    await myBot.run(context);
+  });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
