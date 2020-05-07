bd4ff739f4afd16426c28a8959e9220c5260ed19
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2020-05-07 18:52:33 +0900

    Update bot to test deploy

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ class EchoBot extends ActivityHandler {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            await context.sendActivity(`You said **${ context.activity.text.toUpperCase() }** at ${new Date()}`);
+            await context.sendActivity(`YOU SAID **${ context.activity.text.toUpperCase() }** at ${new Date()}`);
 
             // By calling next() you ensure that the next BotHandler is run.
             await next();
@@ -18,7 +18,7 @@ class EchoBot extends ActivityHandler {
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity('Hello and welcome! http://localhost:3978/auth/signin');
+                    await context.sendActivity('Hello and welcome! http://localhost:3978');
                 }
             }
             // By calling next() you ensure that the next BotHandler is run.
