74e062d6ff3d64f069fa19e4d3659bd49dab8341
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2020-05-08 17:46:38 +0900

    Use LF!

--- bot.js
+++ bot.js
@@ -4,27 +4,27 @@
 const { ActivityHandler } = require('botbuilder');
 
 class EchoBot extends ActivityHandler {
-    constructor() {
-        super();
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-        this.onMessage(async (context, next) => {
-            await context.sendActivity(`YOU SAID **${ context.activity.text.toUpperCase() }** at ${new Date()}`);
+  constructor() {
+    super();
+    // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+    this.onMessage(async (context, next) => {
+      await context.sendActivity(`YOU SAID **${ context.activity.text.toUpperCase() }** at ${new Date()}`);
 
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+    });
 
-        this.onMembersAdded(async (context, next) => {
-            const membersAdded = context.activity.membersAdded;
-            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-                if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity('Hello and welcome! http://localhost:3978');
-                }
-            }
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
-    }
+    this.onMembersAdded(async (context, next) => {
+      const membersAdded = context.activity.membersAdded;
+      for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+        if (membersAdded[cnt].id !== context.activity.recipient.id) {
+          await context.sendActivity('Hello and welcome! http://localhost:3978');
+        }
+      }
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+    });
+  }
 }
 
 module.exports.EchoBot = EchoBot;
